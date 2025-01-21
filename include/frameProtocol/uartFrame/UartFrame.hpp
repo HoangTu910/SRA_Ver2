1273b1046cf3354686ef3ba6a4e24533dfed5ca4
HoangTu910 <tuhoang91003@gmail.com>
2025-01-21 22:40:41 +0700

    frame parsing

--- include/frameProtocol/uartFrame/UartFrame.hpp
+++ include/frameProtocol/uartFrame/UartFrame.hpp
@@ -1,35 +1,93 @@
 #include <stdint.h>
 #include <memory>
+#include <vector>
 #include "CRC16.hpp"
+#include "FrameNumberHelper.hpp"
 
 #define UART_FRAME_MAX_DATA_SIZE 255
 
 /**
  * @brief The data will be construct following this frame before sending to device or receive from device.
  */
+namespace Transmission
+{
+namespace UartFrame
+{
 typedef struct UartFrameData
 {
     uint8_t str_headerHigh;
     uint8_t str_headerLow;
-    uint16_t str_dataLenght;
-    uint16_t str_data[UART_FRAME_MAX_DATA_SIZE];
+    uint8_t str_dataLenghtHigh;
+    uint8_t str_dataLenghtLow;
+    uint8_t str_data[UART_FRAME_MAX_DATA_SIZE];
     uint8_t str_trailerHigh;
     uint8_t str_trailerLow;
-    uint16_t str_crc;
+    uint8_t str_crcHigh;
+    uint8_t str_crcLow;
 } UartFrameData;
 
 class UartFrame
 {
 private:
     std::shared_ptr<UartFrameData> m_uartFrame;
-
+    uint16_t m_dataLength;
+    uint16_t m_crcReceive;
+    std::vector<uint8_t> m_frameBuffer;
+    UartParserState m_parserNextState;
+    UartParserState m_parserFinalState;
 public:
+    /**
+     * @brief Constructor of UartFrame
+     */
     UartFrame();
-    ~UartFrame();
-    void constructFrame();
-    void parseFrame();
 
+    /**
+     * @brief Destructor of UartFrame
+     */
+    ~UartFrame();
+
+    /**
+     * @brief Construct frame from data
+     */
+    void constructFrame();
+
+    /**
+     * @brief Parse each byte of frame and verify the frame
+     */
+    void parseFrame(uint8_t byteFrame);
+
+    /**
+     * @brief Reset parser state
+     */
+    void resetParserState();
+
+    
+    /**
+     * @brief Reset frame buffer
+     */
+    void resetFrameBuffer();
+
+    /**
+     * @brief Check each byte of frame 
+     */
+    bool isFirstHeaderByteValid(uint8_t byteFrame);
+    bool isSecondHeaderByteValid(uint8_t byteFrame);
+    bool isDataLengthFirstByteValid(uint8_t byteFrame);
+    bool isDataLengthSecondByteValid(uint8_t byteFrame);
+    bool isFirstTrailerByteValid(uint8_t byteFrame);
+    bool isSecondTrailerByteValid(uint8_t byteFrame);
+    bool isCrcFirstByteValid(uint8_t byteFrame);
+    bool isCrcSecondByteValid(uint8_t byteFrame);
+    bool isCrcMatched(uint16_t crcReceive);
+    /**
+     * @brief Collect each byte of data
+     */
+    void collectData(uint8_t byteFrame);
+
+    /**
+     * @brief Smart pointer to create UartFrameData object
+     */
     static std::shared_ptr<UartFrameData> create();
 };
-
-
+}
+} // namespace Communication::UartFrame
--- src/frameProtocol/uartFrame/UartFrame.cpp
+++ src/frameProtocol/uartFrame/UartFrame.cpp
@@ -1,12 +1,176 @@
 #include "frameProtocol/uartFrame/UartFrame.hpp"
 
+namespace Transmission
+{
+namespace UartFrame
+{
+
 UartFrame::UartFrame()
 {
 }
+
+
 UartFrame::~UartFrame()
 {
 }
+
+
+void UartFrame::constructFrame()
+{
+    /**
+     * @brief Currently not using this function
+     * @brief The data frame will be sent by STM32 device
+     * @brief Can be used to create mock data frame for transmission testing
+     */
+}
+
+void UartFrame::parseFrame(uint8_t byteFrame)
+{
+    switch (m_parserNextState)
+    {
+        case UartParserState::VERIFY_HEADER_1_BYTE:
+            m_parserNextState = isFirstHeaderByteValid(byteFrame) ? UartParserState::VERIFY_HEADER_2_BYTE : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::VERIFY_HEADER_2_BYTE:
+            m_parserNextState = isSecondHeaderByteValid(byteFrame) ? UartParserState::WAIT_FOR_DATA_LENGTH_1_BYTE : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::WAIT_FOR_DATA_LENGTH_1_BYTE:
+            m_parserNextState = isDataLengthFirstByteValid(byteFrame) ? UartParserState::WAIT_FOR_DATA_LENGTH_2_BYTE : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::WAIT_FOR_DATA_LENGTH_2_BYTE:
+            m_parserNextState = isDataLengthSecondByteValid(byteFrame) ? UartParserState::RECEIVE_DATA : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::RECEIVE_DATA:
+            collectData(byteFrame);
+            m_frameBuffer.size() == m_dataLength ? UartParserState::WAIT_FOR_CRC_1_BYTE : UartParserState::RECEIVE_DATA;
+            break;
+        case UartParserState::VERIFY_TRAILER_1_BYTE:
+            m_parserNextState = isFirstTrailerByteValid(byteFrame) ? UartParserState::VERIFY_TRAILER_2_BYTE : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::VERIFY_TRAILER_2_BYTE:
+            m_parserNextState = isSecondTrailerByteValid(byteFrame) ? UartParserState::FRAME_COMPLETE : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::WAIT_FOR_CRC_1_BYTE:
+            m_parserNextState = isCrcFirstByteValid(byteFrame) ? UartParserState::WAIT_FOR_CRC_2_BYTE : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::WAIT_FOR_CRC_2_BYTE:
+            m_parserNextState = isCrcSecondByteValid(byteFrame) ? UartParserState::VERIFY_CRC : UartParserState::FRAME_ERROR;
+            break;
+        case UartParserState::VERIFY_CRC:
+            uint16_t crcCalculatedFromData = CRC16::calculateCRC(m_frameBuffer.data(), m_frameBuffer.size());
+            m_parserNextState = isCrcMatched(crcCalculatedFromData) ? UartParserState::FRAME_COMPLETE : UartParserState::FRAME_ERROR;
+            break;
+    }
+    m_parserFinalState = m_parserNextState;
+}
+
+void UartFrame::resetParserState()
+{
+    m_parserNextState = UartParserState::VERIFY_HEADER_1_BYTE;
+}
+
+void UartFrame::resetFrameBuffer()
+{
+    m_frameBuffer.clear();
+}
+
+bool UartFrame::isFirstHeaderByteValid(uint8_t byteFrame)
+{
+    if (byteFrame == UartFrameConstants::UART_FRAME_HEADER_1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isSecondHeaderByteValid(uint8_t byteFrame)
+{
+    if (byteFrame == UartFrameConstants::UART_FRAME_HEADER_2)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isDataLengthFirstByteValid(uint8_t byteFrame)
+{
+    if (byteFrame <= UART_FRAME_MAX_DATA_SIZE)
+    {
+        m_dataLength = byteFrame;
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isDataLengthSecondByteValid(uint8_t byteFrame)
+{
+    if (byteFrame <= UART_FRAME_MAX_DATA_SIZE)
+    {
+        m_dataLength = (m_dataLength << 8) | byteFrame;
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isFirstTrailerByteValid(uint8_t byteFrame)
+{
+    if (byteFrame == UartFrameConstants::UART_FRAME_TRAILER_1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isSecondTrailerByteValid(uint8_t byteFrame)
+{
+    if (byteFrame == UartFrameConstants::UART_FRAME_TRAILER_2)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isCrcFirstByteValid(uint8_t byteFrame)
+{
+    if (byteFrame <= BYTE_MAX)
+    {
+        m_crcReceive = byteFrame;
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isCrcSecondByteValid(uint8_t byteFrame)
+{
+    if (byteFrame <= BYTE_MAX)
+    {
+        m_crcReceive = (m_crcReceive << 8) | byteFrame;
+        return true;
+    }
+    return false;
+}
+
+bool UartFrame::isCrcMatched(uint16_t crcCalculatedFromData)
+{
+    if (m_crcReceive == crcCalculatedFromData)
+    {
+        return true;
+    }
+    return false;
+}
+
+void UartFrame::collectData(uint8_t byteFrame)
+{
+    if(m_frameBuffer.size() == UART_FRAME_MAX_DATA_SIZE)
+    {
+        return;
+    }
+    m_frameBuffer.push_back(byteFrame);
+}
+
 std::shared_ptr<UartFrameData> UartFrame::create()
 {
     return std::shared_ptr<UartFrameData>();
 }
+} 
+} // namespace Transmission::UartFrame
