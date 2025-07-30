abc67b8e4ce29c2f16eee4447e418249b4c7eed0
HoangTu910 <tuhoang91003@gmail.com>
2025-07-30 16:12:14 +0700

    save 30/7

--- src/communication/CommunicationNumberHelper.cpp
+++ src/communication/CommunicationNumberHelper.cpp
@@ -3,15 +3,25 @@
 
 namespace WifiHelper
 {
-    char* SSID = (char*)"your_wifi_ssid";
-    char* PASSWORD = (char*)"your_wifi_password";
-    int WIFI_TIMEOUT = 5000;
-}
+    // char *SSID = (char*)"The Jade Coffee and Tea";
+    // char *PASSWORD = (char*)"caphengon";
 
+    // char *SSID = (char*)"Dau Cafe Ngoai Troi";
+    // char *PASSWORD = (char*)"Mocua24h";
+
+    char *SSID = (char*)"Hoang Tuan";
+    char *PASSWORD = (char*)"03081973";
+
+    // char *SSID = (char*)"FETEL@DESLAB_SV";
+    // char *PASSWORD = (char*)"DeslabSV";
+
+    // char *SSID = (char*)"Redmi Note 11S";
+    // char *PASSWORD = (char*)"hoangtu2003";
+}
 
 namespace MQTTHelper
 {   
-    char* MQTT_SERVER = (char*)"192.168.1.58";
+    char* MQTT_SERVER = (char*)"192.168.1.5";
     int MQTT_PORT = 1885;
     char* MQTT_USER = (char*)"admin";
     char* MQTT_PASSWORD = (char*)"123";
